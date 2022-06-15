4bce65360bc25d7fdcf6737870cedf147f181087
Olamiposi <56056759+posigit@users.noreply.github.com>
2022-06-15 21:35:55 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,50 @@
+// if (7 > 5) {
+//     document.write("This statement is true")
+// }
+
+
+
+// var myNum = 20;
+
+// if (myNum > 19) {
+//     document.write ("Fuck you");
+// }
+
+// if (myNum == 20) {
+//     document.write ("This number is equal to twenty");
+// } else {
+//     document.write ("This number is not equal to twenty");
+// }
+
+// var youLikeMeat = false;
+
+// if (youLikeMeat) {
+//     document.write ("You like meat");
+// } else {
+//     document.write ("You hate meat");
+// }
+
+// var myAge = 29;
+
+// if (myAge > 29) {
+//     document.write ("You are over 30!");
+// }
+// else if (myAge > 19) {
+//     document.write ("You are over 20!");
+// }
+// else if (myAge > 9) {
+//     document.write ("You are over 10!");
+// } 
+// else{
+//     document.write ("You are not over 10!");
+// }
+
+var myAge = 18;
+
+if (myAge >= 18 && myAge <=30){
+    document.write("You cool dude!")
+}
+
+else {
+    document.write ("You ain't coming!")
+}
